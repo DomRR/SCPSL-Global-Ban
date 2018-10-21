c2f04ca5bfdb842274581230151212c3191ac868
DomRR <2974504231@qq.com>
2018-10-21 15:25:46 +0800

    添加项目文件。

---
+++ ExamplePlugin/ListUpdateThread.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+namespace SCPSL_Global_Ban
+{
+    internal class ListUpdateThread
+    {
+        public bool MyRemoteCertificateValidationCallback(System.Object sender,
+    X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            bool isOk = true;
+            if (sslPolicyErrors != SslPolicyErrors.None)
+            {
+                for (int i = 0; i < chain.ChainStatus.Length; i++)
+                {
+                    if (chain.ChainStatus[i].Status == X509ChainStatusFlags.RevocationStatusUnknown)
+                    {
+                        continue;
+                    }
+                    chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+                    chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                    chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+                    chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+                    bool chainIsValid = chain.Build((X509Certificate2)certificate);
+                    if (!chainIsValid)
+                    {
+                        isOk = false;
+                        break;
+                    }
+                }
+            }
+            return isOk;
+        }
+        public ListUpdateThread()
+        {
+            ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
+
+            // ID
+            var web = WebRequest.Create("https://scpsl.net/Global_Ban/api.php?method=id");
+            web.Method = "GET";
+            var stream = new StreamReader(web.GetResponse().GetResponseStream());
+            string str = stream.ReadToEnd();
+            string[] str_Array = str.Split(new[] { "<br>" }, StringSplitOptions.None);
+            foreach (string s in str_Array)
+            {
+                if (Int64.TryParse(s, out Int64 id))
+                {
+                    Events.ban_id.Add(id);
+                }
+            }
+
+            // IP
+            web = WebRequest.Create("https://scpsl.net/Global_Ban/api.php?method=ip");
+            web.Method = "GET";
+            stream = new StreamReader(web.GetResponse().GetResponseStream());
+            str = stream.ReadToEnd();
+            str_Array = str.Split(new[] { "<br>" }, StringSplitOptions.None);
+            foreach (string s in str_Array)
+            {
+                Events.ban_ip.Add(s);
+            }
+        }
+    }
+}
---
+++ ExamplePlugin/SCPSL_Global_Ban.cs
@@ -0,0 +1,79 @@
+﻿using Smod2;
+using Smod2.API;
+using Smod2.Attributes;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using System.Threading;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+using System.Net;
+
+namespace SCPSL_Global_Ban
+{
+    [PluginDetails(
+        author = "DomRR",
+        name = "SCPSL Global Ban",
+        description = "",
+        id = "rbq.global.ban",
+        version = "1.01",
+        SmodMajor = 3,
+        SmodMinor = 0,
+        SmodRevision = 0
+        )]
+    class Events : Plugin, IEventHandlerRoundStart, IEventHandlerPlayerJoin
+    {
+        public static List<Int64> ban_id = new List<Int64>();
+        public static List<string> ban_ip = new List<string>();
+        public override void OnDisable()
+        {
+        }
+        public override void OnEnable()
+        {
+
+            Info("SCPSL Global Ban v1.01 插件已加载 :)");
+        }
+        public override void Register()
+        {
+            Thread updateThread = new Thread(new ThreadStart(() => new ListUpdateThread()));
+            updateThread.Start();
+            AddEventHandler(typeof(IEventHandlerPlayerJoin), this, Priority.Normal);
+            AddEventHandler(typeof(IEventHandlerRoundStart), this, Priority.Normal);
+        }
+        public void OnRoundStart(RoundStartEvent ev)
+        {
+            ban_id = new List<Int64>();
+            ban_ip = new List<string>();
+            Info("正在更新数据.");
+            Thread updateThread = new Thread(new ThreadStart(() => new ListUpdateThread()));
+            updateThread.Start();
+        }
+
+        public void OnPlayerJoin(PlayerJoinEvent ev)
+        {
+            Player player = ev.Player;
+            Int64.TryParse(player.SteamId, out Int64 id);
+            string ip = player.IpAddress.Split(':')[3];
+            /*Info(ip);
+            foreach (Int64 i in ban_id)
+            {
+                Info(i.ToString());
+            }
+            foreach (string s in ban_ip)
+            {
+                Info(s);
+            }*/
+            if (ban_id.Contains(id))
+            {
+                Info("踢出被封禁的 Id: " + id.ToString());
+                player.Ban(1);
+            }
+            if (ban_ip.Contains(ip))
+            {
+                Info("踢出被封禁的 IP: " + ip);
+                player.Ban(1);
+            }
+        }
+    }
+}
